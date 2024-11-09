5213638338edc5ed31f161269cc329263da7d6d9
Sourav Bapari <codexsourav404@gmail.com>
2024-11-09 19:45:25 +0530

    update

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,7 @@
 // lib/apolloClient.js
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-export const strApi = "http://194.238.19.82:1337";
+export const strApi = "https://g2g-cms.souravbapari.in";
 // Load the API key from environment variables
 const API_KEY =
   "52532a015f85d899866b18d8c883d75aaeff795483d85b45595db9b0691abe889a41034c9a0acd6d102ff2f4e58d461ed29f768cef2bdc1448c4a9923049429af7bb80b5516a389bc02308532cab7a451999bb2c2cc03096d054384fb3a3155d577b85890d002f9ce571a7bc78b8188be6a01d1dfdb67acff3d8eaf9ab6c16af"; // Make sure to define this in .env.local
