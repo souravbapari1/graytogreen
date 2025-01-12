513486fded267a9e35b2c3c0159ee1b8f1daa610
sourav <codexsourav404@gmail.com>
2025-01-12 06:00:45 +0530

    update

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-import data from "./csvjson.json";
-for (let i = 0; i < data.length; i++) {
-  const element = data[i];
-  console.log(element);
-}
+// import data from "./csvjson.json";
+// for (let i = 0; i < data.length; i++) {
+//   const element = data[i];
+//   console.log(element);
+// }
